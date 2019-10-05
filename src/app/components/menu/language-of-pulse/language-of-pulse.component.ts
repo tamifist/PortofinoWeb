b3c230eac1dcadd26482290c44d8038f75417b52
Denis <ts-alan@mail.ru>
2019-10-05 14:57:51 +0300

    add logic submenus and images

--- src/app/components/menu/language-of-pulse/language-of-pulse.component.ts
+++ src/app/components/menu/language-of-pulse/language-of-pulse.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {map, shareReplay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-language-of-pulse',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./language-of-pulse.component.scss']
 })
 export class LanguageOfPulseComponent implements OnInit {
+  isHandset: boolean;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      ).subscribe((result) => {
+      this.isHandset = result;
+    });
+  }
 
   ngOnInit() {
+
   }
 
 }
