eada5182315e01165aea3e460e3ac2f8d36c1726
Vlad Omelyanchuk <v.omelyanchuk@outlook.com>
2019-10-06 14:15:21 +0300

    minor changes

--- src/app/components/menu/managing-your-account/managing-your-account.component.ts
+++ src/app/components/menu/managing-your-account/managing-your-account.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgbCarousel} from '@ng-bootstrap/ng-bootstrap';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {map, shareReplay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-managing-your-account',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./managing-your-account.component.scss']
 })
 export class ManagingYourAccountComponent implements OnInit {
+  isHandset: boolean;
+  showNextScreen = true;
+  @ViewChild(NgbCarousel, {static : false }) NgbCarouselElement: NgbCarousel ;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      ).subscribe((result) => {
+      this.isHandset = result;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  carouselNext() {
+    if (Number(this.NgbCarouselElement.activeId.replace(/\D/g, ''))
+      === (Number(this.NgbCarouselElement.slides.last.id.replace(/\D/g, '')) - 1 ) ) {
+      this.showNextScreen = false;
+    }
+
+    this.NgbCarouselElement.next();
   }
 
 }
