e86f503ba5b9d36d9f6dc386a6374e242f71efa2
Vlad Omelyanchuk <v.omelyanchuk@outlook.com>
2019-10-06 19:05:52 +0300

    minor changes

---
+++ src/app/components/menu/finding-your-way-around-pulse-app/finding-your-way-around-pulse-app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import {BaseComponent} from '../../base/base.component';
+
+@Component({
+  selector: 'app-finding-your-way-around-pulse-app',
+  templateUrl: './finding-your-way-around-pulse-app.component.html',
+  styleUrls: ['./finding-your-way-around-pulse-app.component.scss']
+})
+export class FindingYourWayAroundPulseAppComponent extends BaseComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
