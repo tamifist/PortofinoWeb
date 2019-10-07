0f0d94d681fe393d40f6b195f50a6f64b9214e6b
Vlad Omelyanchuk <tamifist@gmail.com>
2019-10-07 14:15:01 +0300

    Rework Language of Pulse

--- src/app/components/menu/language-of-pulse/language-of-pulse.component.ts
+++ src/app/components/menu/language-of-pulse/language-of-pulse.component.ts
@@ -1,27 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {map, shareReplay} from 'rxjs/operators';
+import {BaseComponent} from '../../base/base.component';
 
 @Component({
   selector: 'app-language-of-pulse',
   templateUrl: './language-of-pulse.component.html',
   styleUrls: ['./language-of-pulse.component.scss']
 })
-export class LanguageOfPulseComponent implements OnInit {
-  isHandset: boolean;
-
-  constructor(private breakpointObserver: BreakpointObserver) {
-    this.breakpointObserver.observe(Breakpoints.Handset)
-      .pipe(
-        map(result => result.matches),
-        shareReplay()
-      ).subscribe((result) => {
-      this.isHandset = result;
-    });
-  }
-
-  ngOnInit() {
-
-  }
-
+export class LanguageOfPulseComponent extends BaseComponent {
 }
