18850e60496b0c02202bb8f466f1116fec0c54d5
Denis <ts-alan@mail.ru>
2019-10-08 19:41:52 +0300

    add transfer link

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,17 +1,22 @@
-import {Component, Renderer2} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map, shareReplay } from 'rxjs/operators';
+import {filter, map, shareReplay} from 'rxjs/operators';
+import {NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss']
 })
-export class MainNavComponent {
+
+export class MainNavComponent implements  AfterViewInit {
+
   isHandset: boolean;
+  @ViewChild('matnavlist', {static : false }) matnavlist: ElementRef ;
 
   constructor(private breakpointObserver: BreakpointObserver,
-              private render: Renderer2) {
+              private render: Renderer2,
+              private router: Router) {
     this.breakpointObserver.observe(Breakpoints.Handset)
       .pipe(
         map(result => result.matches),
@@ -19,15 +24,32 @@ export class MainNavComponent {
       ).subscribe((result) => {
      this.isHandset = result;
     });
+
+
+
+
   }
 
-  changeClassForMenu(event) {
-    // clear class active from another elements
-    event.currentTarget.childNodes.forEach((currentValue, index, array) => {
-      this.render.removeClass(currentValue.childNodes[0], 'active');
+  ngAfterViewInit(): void {
+
+    let childrenItems = this.matnavlist.nativeElement.children[0].children;
+
+    for ( let i = 0; i < childrenItems.length ; i++ ) {
+      if (this.router.url.match(childrenItems[i].pathname)) {
+        this.render.addClass(childrenItems[i].children[0], 'active');
+      }
+    }
+
+    this.router.events.pipe(filter(event => event instanceof NavigationStart)).subscribe((event :any) => {
+      for ( let i = 0; i < childrenItems.length ; i++ ) {
+        this.render.removeClass(childrenItems[i].children[0], 'active');
+      }
+
+      for ( let i = 0; i < childrenItems.length ; i++ ) {
+        if (childrenItems[i].href.toString().match(event.url)) {
+          this.render.addClass(childrenItems[i].children[0], 'active');
+        }
+      }
     });
-
-    this.render.addClass(event.target, 'active');
   }
-
 }
