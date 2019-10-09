9c1b825c76e644b01032ba5129f9f0519913fee6
Vlad Omelyanchuk <tamifist@gmail.com>
2019-10-09 21:02:05 +0300

    ui fixes

--- src/app/components/menu/pulse-for-corporates/pulse-for-corporates.component.ts
+++ src/app/components/menu/pulse-for-corporates/pulse-for-corporates.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {BaseComponent} from '../../base/base.component';
 
 @Component({
   selector: 'app-pulse-for-corporates',
   templateUrl: './pulse-for-corporates.component.html',
   styleUrls: ['./pulse-for-corporates.component.scss']
 })
-export class PulseForCorporatesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PulseForCorporatesComponent extends BaseComponent {
 }
